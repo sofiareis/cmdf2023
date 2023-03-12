007fa531517fa41e6d3761fb3824c30c781b02f1
sofiareis <sofiareis2018@gmail.com>
2023-03-11 16:54:16 -0800

    routes and landing init

--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -1,13 +1,27 @@
 import React from "react";
 import './Landing.css'
+import image from './image.svg'
 function Landing() {
     return (
         <div className="Landing">
               <header className="Landing-header">
-                <p>
-                  Interview Helper!
-                </p>
+                <div className="Landing-title">
+                  <div className="Landing-comp">
+                    <div className="Landing-wrap">
+                      <h1 className="Landing-h1">
+                        CareerCue
+                      </h1>
+                      <p className="Landing-h1-desc">
+                        Some words here
+                      </p>
+                    </div>
+                  </div>
+                  <img src={image} className="Landing-logo" alt="image" />
+                </div>
               </header>
+              <body className="Landing-about">
+
+              </body>
             </div>
     )
 }
